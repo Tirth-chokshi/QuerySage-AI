43e6fdcd1d80965d59b1a71f9bb580c3208e47ba
Tirth chokshi <chokshitirth4@gmail.com>
2024-08-01 19:56:14 +0530

    trying to fix prod

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+"use clientp"
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 "use client"
 import { ContainerScroll } from '@/components/ui/container-scroll-animation'
 import Navbar from '@/components/Navbar'
-
+import AnimatedGridPattern from '@/components/magicui/animated-grid-pattern'
+import { cn } from '@/lib/utils'
 export default function Home() {
   return (
     <div className="container mx-auto p-4">
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
       <Navbar />
       <ContainerScroll titleComponent={"querysage"} />
     </div>
