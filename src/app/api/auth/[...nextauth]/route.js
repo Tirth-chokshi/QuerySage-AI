ac28596baa97d974fc1f674a7d0d6cb99af296b2
Tirth chokshi <chokshitirth4@gmail.com>
2024-08-02 20:17:48 +0530

    auth prod fix2

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-"use client"
+// "use client"
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
@@ -16,11 +16,6 @@ const handler = NextAuth({
   ],
   session: {
     strategy: "jwt",
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl
-    }
   },
   secret: process.env.AUTH_SECRET,
 })
