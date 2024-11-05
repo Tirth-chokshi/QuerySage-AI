e88e9c2cb8021eecf1445d03e29bf104be025669
Tirth chokshi <chokshitirth4@gmail.com>
2024-11-05 18:43:09 +0530

    footer removed temp

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {children}
-          <Footer/>
+          {/* <Footer/> */}
         </ThemeProvider>
         </SessionWrapper>
       </body>
